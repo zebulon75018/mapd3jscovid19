78caa31cd645d6d0ce17e754c1e3121149a19130
zebulon75018 <zebulon75018@yahoo.fr>
2020-05-02 13:44:11 +0200

    add other route from data

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import render_template
 import csv
+import json 
+
 
 
 app = Flask(__name__)
@@ -54,6 +56,35 @@ with open("sursaud-covid19-quotidien-2020-04-16-10h47-departement.csv") as csvfi
 #print(departement)
 
 
+with open('timeseries.json') as f:
+  data = json.load(f)
+
+death = {}
+
+for country in data.keys():
+    death[country] = []        
+    for i in range(1,len(data[country])):        
+        dd = {"date":  data[country][i]["date"] ,
+                                "deaths": data[country][i]["deaths"]-data[country][i-1]["deaths"],
+                                "confirmed": data[country][i]["confirmed"]-data[country][i-1]["confirmed"],
+                                "recovered": data[country][i]["recovered"]-data[country][i-1]["recovered"]
+                                 }        
+        death[country].append(dd)        
+
+treemap = []
+
+for country in data.keys():    
+    lastindex = len(data[country]) -1    
+    if data[country][lastindex]["deaths"]<100: 
+        continue
+
+    dd = {                  "name" : country,
+                            "deaths": data[country][lastindex]["deaths"],
+                            "confirmed": data[country][lastindex]["confirmed"],
+                            "recovered": data[country][lastindex]["recovered"]
+                                }        
+    treemap.append(dd)
+
 class DataCovid :
     def __init__(self, filename):
         self.data = {}
@@ -95,6 +126,25 @@ datacovid = DataCovid("time_series_covid19_deaths_global.csv.txt")
 @app.route('/favicon.ico')
 def favicon():
     return ""
+
+
+
+@app.route('/treemap')
+def treemapr():
+    return render_template('treemap.html', country =treemap)    
+
+@app.route('/all')
+def all():
+    return render_template('all.html', country =data)    
+
+@app.route('/death')
+def deathroute():
+    return render_template('death.html', country =death)    
+
+@app.route('/')
+def home():
+    return render_template('index.html', country =data)    
+
 @app.route('/usa/')
 def usa():
     return render_template('usa.html',covid=covidUSA)    
@@ -102,11 +152,11 @@ def usa():
 @app.route('/france/')
 def france():
     return render_template('france.html',covidFR=covidFR)    
-@app.route('/')
-def home():
+@app.route('/world/')
+def world():
     return render_template('map.html',data = datacovid, iddate = 4)    
 
-@app.route('/<iddate>')
+@app.route('/world/<iddate>')
 def withdate(iddate):
     return render_template('map.html',data = datacovid,iddate=iddate)    
 
